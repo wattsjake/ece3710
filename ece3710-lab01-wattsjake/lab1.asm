d49e6597fb280c1551897c7ff12287e7cb98a388
wattsjake <watts.jacob.samuel@gmail.com>
2023-01-18 09:48:40 -0700

    fixed assembly code 4 errors

--- ece3710-lab01-wattsjake/lab1.asm
+++ ece3710-lab01-wattsjake/lab1.asm
@@ -14,8 +14,8 @@ str_x:  ds      10h             ; reserve 16 bytes
 flag:   dbit    1               ; reserve 1 bit for flag 
  
         cseg 
-        mov     wdtcn,#DEh      ; Disable watchdog 
-        mov     wdtcn,#DEh      ; Disable watchdog
+        mov     wdtcn,#0DEh      ; Disable watchdog for hex values you need to have a zero infront
+        mov     wdtcn,#0ADh      ; Disable watchdog
         mov     r0,#255 
 clrall: mov     @r0,#0          ; clear all internal ram 
         djnz    r0,clrall 
@@ -24,7 +24,7 @@ clrall: mov     @r0,#0          ; clear all internal ram
         mov     dptr,#str_x     ; point dptr at str_x 
         mov     r0,#str_d       ; point r0 at str_d 
         mov     r1,#str_i       ; point r1 at str_i 
-        mov     r6,#10          ; copy all the bytes 
+        mov     r6,#10h          ; copy all the bytes 
         mov     r7,#str_c-pc_rel; pc relative offset to str_c 
 loop1:  mov     a,r7            ; offset to first (next) byte 
         movc    a,@a+pc         ; actually get the byte 
@@ -32,6 +32,7 @@ pc_rel: mov     @r0,a           ; store in str_d
         mov     @r1,a           ; store in str_i 
         movx    @dptr,a         ; store in str_x 
         inc     r0 
+				inc 		r1
         inc     dptr            ; increment pointers 
         inc     r7 
         djnz    r6,loop1        ; loop 16 times 
