add8a9e0f607257ca9c1e1078f3167ee285c41aa
wattsjake <watts.jacob.samuel@gmail.com>
2023-01-24 00:14:36 -0700

    added new schematic

--- TODO.md
+++ TODO.md
@@ -2,4 +2,7 @@ TODO
 ====
 
 * print documentation create spiral bound book
-* 
+* check .ignore files when you create another project
+* figure out how to cross out TODO txt
+
+
--- ece3710-lab02-wattsjake/lab02.asm
+++ ece3710-lab02-wattsjake/lab02.asm
@@ -4,4 +4,17 @@ $include (c8051f020.inc)
         mov wdtcn,#0ADh 
         mov xbr2,#40h ; enable port output
 
+				cseg
+loop1:	mov P5,P2 ; P2 - dip switch, P5 LED lights
+				mov C, P1.0
+				mov P3.0, C
+				mov C, P1.1
+				mov P3.1, C
+
+				jmp	loop1
+
+				END
+
+
+
         
