c2b00aa6bddf379a00ce719e69cd3a81af8c703c
wattsjake <watts.jacob.samuel@gmail.com>
2023-01-26 13:37:02 -0700

    added script.py

--- python/script.py
+++ python/script.py
@@ -1,36 +1,57 @@
-import sys
+import glob
+import json
 import os
+import sys
 import time
 
-# Check if the user has provided the correct number of arguments
-if len(sys.argv) != 2:
-    print("Usage: python script.py <foldername>")
-    sys.exit(0)
+def parent_dir():
+    # Get the parent directory of the current directory
+    return os.path.dirname(os.getcwd())
 
-# Get the parent directory of the current directory
-parent_dir = os.path.dirname(os.getcwd())
-print("Parent Directory: {}".format(parent_dir))
+def create_folder(idx):
+    # Create the folder
+    foldername = "ece3710-lab" + str(idx) + "-wattsjake"
 
-# get folder number from command line and create foldername
-idx = sys.argv[1]
-foldername = "ece3710-lab" + str(idx) + "-wattsjake"
-print("Filename: {}".format(foldername))
+def check_folder_exists(foldername):
+    #check to see if folder already exists
+    if os.path.exists(os.path.join(parent_dir, foldername)):
+        print("Folder already exists")
+        return True
+    else:
+        return False
 
-#check to see if folder already exists
-if os.path.exists(os.path.join(parent_dir, foldername)):
-    print("Folder already exists")
-    sys.exit(0)
-#otherwise create folder
-else:
-    os.mkdir(os.path.join(parent_dir, foldername))
+def load_json():
+    json_file = glob.glob('*.json') # get json file
+    # Opening JSON file
+    with open(str(json_file[0]), 'r') as json_file:
+        # Reading from json file
+        json_object = json.load(json_file)
+    return json_object
 
-# Create README.md file inside foldername
-with open(os.path.join(parent_dir, foldername, "README.md"), "w") as f:
-    #print date in ISO 8601 format with lab number
-    f.write("# Lab " + str(idx) + " - Created - " + time.strftime("%Y-%m-%d", time.gmtime()))
+def user_input():
+    # Check if the user has provided the correct number of arguments
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <foldername>")
+        sys.exit(0)
 
-# Create docs folder inside foldername
-os.mkdir(os.path.join(parent_dir, foldername, "docs"))
+    # Check if the user has provided the help argument
+    if (sys.argv[1] == "-help" or sys.argv[1] == "-h"):
+        print("Usage: python script.py <foldername>")
+        sys.exit(0)
+
+    #Get the folder number from the command line
+    return sys.argv[1]
+
+def main():
+    print("Creating folder...")
+    idx = user_input()
+    print(idx)
+
+
+if __name__ == "__main__":
+     main()
+
+
 
 
 
