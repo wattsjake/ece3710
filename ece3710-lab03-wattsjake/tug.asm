c82c838550d44287cfc1f14143792b4d896b3297
wattsjake <watts.jacob.samuel@gmail.com>
2023-01-27 10:15:20 -0700

    added tugowar.asm

--- ece3710-lab03-wattsjake/tug.asm
+++
@@ -1,7 +0,0 @@
-$include (c8051f020.inc) 
-
-        mov wdtcn,#0DEh ; disable watchdog 
-        mov wdtcn,#0ADh
-
-
-
---
+++ ece3710-lab03-wattsjake/tugowar.asm
@@ -0,0 +1,20 @@
+$include (c8051f020.inc) 
+
+        mov wdtcn,#0DEh ; disable watchdog 
+        mov wdtcn,#0ADh
+
+				mov xbr2,#40h ; enable port output
+
+				cseg
+
+loop1:  mov P5,P2 ; P2 - dip switch, P5 LED lights
+				mov C, P1.0
+				mov P3.0, C
+				mov C, P1.1
+				mov P3.1, C
+				jmp loop1
+
+				END
+
+
+
