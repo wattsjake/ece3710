62af8aac257bbd51e202fc4fac604bd588fe3f4d
wattsjake <watts.jacob.samuel@gmail.com>
2023-01-30 22:58:12 -0700

    added pdf

--- ece3710-lab03-wattsjake/src/tugowar.asm
+++ ece3710-lab03-wattsjake/src/tugowar.asm
@@ -1,3 +1,11 @@
+;******************************************************************************
+;*  Course: ECE 3710 Embedded Systems										  *
+;*  Assignment: Lab03                                                         *
+;*  Author:  Jack Fernald                                                     *
+;*  Verified: Jacob Watts                                                     *
+;*  Date: 2023-01-30                                                          *
+;******************************************************************************
+
 $include (c8051f020.inc) 
 
         mov wdtcn,#0DEh ; disable watchdog 
@@ -5,25 +13,22 @@ $include (c8051f020.inc)
         mov xbr2,#40h ; enable port output
 
 				DSEG AT 30H
-last_button: ds 1
-game_state: ds 1
+last_button: 	ds 1
+game_state: 	ds 1
 
 
-				CSEG
-
-
-
-
-;; TODO
-;; - Add routine for controlling state
-
-
-
+							 CSEG
 Main:          CALL Initilize
 start:         MOV R4, #10
                CALL Delay
 							 MOV A, game_state
-							 CALL LED_driver
+
+							 CJNE A, #00h, THERE
+							 JMP Main
+THERE: 				 CJNE A, #08h, HERE
+							 JMP Main
+							 
+HERE:					 CALL LED_driver
                CALL check_buttons
 							 CJNE A, #01, check_btn2
                INC game_state
@@ -31,9 +36,6 @@ start:         MOV R4, #10
 check_btn2:    CJNE A, #02,  start
                DEC game_state
 							 JMP start
-
-
-
 
 check_buttons: MOV A, P1
 							 CPL A
@@ -45,7 +47,6 @@ check_buttons: MOV A, P1
 P1_table:      DB 0FCh, 0F9h, 0F3h, 0E7h, 0CFh, 09Fh, 03Fh, 07Fh, 0FFh ; 0 1 2 3 4 5 6 7
 P2_table:      DB 0FEh, 0FCh ; 8 9
 
-
 LED_driver:    CJNE A, #07h, NEXT1
 							 MOV R4, #07fh
 							 MOV P5, R4
@@ -54,14 +55,12 @@ LED_driver:    CJNE A, #07h, NEXT1
 NEXT1:
                CJNE A, #08h, LED_helper
 
-
 NEXT:					 SUBB A, #7
                CALL Turn_off
 							 MOV dptr, #P2_table
 							 MOVC A, @A+dptr
 							 MOV P3, A
-							 RET
-               
+							 RET       
 							 
 LED_helper:		 MOV  dptr, #P1_table
                MOVC A, @A+dptr
@@ -76,13 +75,8 @@ Turn_off:      JZ Fast_forward
 Fast_forward:  
 							 RET
 
-
-							 
-
 							 RET
                
-							 
-
 Delay:         
 delay_loop_out:MOV R3, #250
 delay_loop_in: NOP
@@ -106,10 +100,6 @@ Initilize:     MOV game_state, #4
 							 MOV C, 1B
 							 MOV P3.0, C
 							 RET
-
-
-
-
 
                jmp start ; Need a flag here for win state
 
