f3c30eafdce80449a84831264abfc44d0580e57d
wattsjake <watts.jacob.samuel@gmail.com>
2023-02-07 13:46:09 -0700

    inital commit of sumo.asm

---
+++ ece3710-project01-wattsjake/TODO.md
@@ -0,0 +1,6 @@
+TODO - Project01:
+=================
+
+* [x] Add project description to README.md (added to docs)
+* [ ] Fix python script to create files with headers
+* [ ] Add tab to lab book and write objectives
---
+++ ece3710-project01-wattsjake/src/sumo.asm
@@ -0,0 +1,16 @@
+;***************************************************************************
+;Program Name:  "sumo.asm"
+;Description:   A two-player game using the bar graph and two push 
+;               buttons. Each player will control one of two sumo 
+;               wrestlers and each will try to push the 
+;               other out of the ring.
+;
+;Author:        Jacob Watts
+;Organization:  Weber State University ECE 3710
+;Revision History
+;Date[YYYYMMDD] Author      Description
+;----           ------      -----------
+;20230207       Jacob W.    initial commit
+;***************************************************************************
+
+
