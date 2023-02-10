5105feb9297595aa4adf32a7c6ae036c8783a7a1
wattsjake <watts.jacob.samuel@gmail.com>
2023-02-09 22:05:10 -0700

    added to comments on sumo

--- ece3710-project01-wattsjake/src/sumo.asm
+++ ece3710-project01-wattsjake/src/sumo.asm
@@ -35,9 +35,11 @@ sumo2: 	 ds 1
         cseg
         mov A, P2 ;DIP switches 
         anl A, #7 ;use only the first three (3) switches
-        mov sumo1, A
+        mov sumo1, A 
         inc A
-        mov sumo2, A
+        mov sumo2, A ;status of DIP SW inc A and save
+        
+
 
 
 loop:   jmp loop
