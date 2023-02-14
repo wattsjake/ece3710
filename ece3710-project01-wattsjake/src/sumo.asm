7047013d13a46657e706901dd41b6964d4769543
wattsjake <watts.jacob.samuel@gmail.com>
2023-02-14 13:47:42 -0700

    added to project for jack

--- ece3710-project01-wattsjake/src/sumo.asm
+++ ece3710-project01-wattsjake/src/sumo.asm
@@ -17,34 +17,39 @@
 ;20230209       Jacob W.    added sumo1 & 2
 ;********************************************************
 $include (c8051f020.inc) 
+
+        DSEG AT 30H
+sumo1: 	ds 1
+sumo2: 	ds 1
+        
+        CSEG
         mov wdtcn,#0DEh ; disable watchdog 
         mov wdtcn,#0ADh 
         mov xbr2,#40h ; enable port output
 
-;clear all internal ram 
+        ;clear all internal ram 
         mov     r0,#255 
 clrall: mov     @r0,#0
         djnz    r0,clrall
-
 ;---------------PLACE CODE BELOW THIS LINE---------------
 
-        DSEG AT 30H
-sumo1: 	 ds 1
-sumo2: 	 ds 1
-
-        cseg
-        mov A, P2 ;DIP switches 
+init:   mov A, P2 ;DIP switches 
         anl A, #7 ;use only the first three (3) switches
         mov sumo1, A 
         inc A
         mov sumo2, A
 
+check_buttons: MOV A, P1
+               CPL A
+               XCH A, last_button
+               XRL A, last_button
+               ANL A, last_button
+               RET
 
 
-loop:   jmp loop
+loop1:  jmp loop1
         END
-
-        this =
+        
 
 
 
