dcdf19cb0c396d83203b17d03d92cd15cf9c375a
Jack <jackfernald@mail.weber.edu>
2023-02-18 02:17:49 -0700

    Got game in working condition, but glitchy AF

--- ece3710-project01-wattsjake/src/sumo.asm
+++ ece3710-project01-wattsjake/src/sumo.asm
@@ -15,7 +15,9 @@
 ;20230209       Jacob W.        added clear RAM
 ;20230209       Jacob W.        add Jack F. to author
 ;20230209       Jacob W.        added sumo1 & 2
-;20230214       Jacob W.        added 1000ms delay                
+;20230214       Jacob W.        added 1000ms delay   
+;20230218       Jack F.         Got game in working cond-
+;                               -ition but glitchy             
 ;********************************************************                
 $include (c8051f020.inc) 
 
@@ -28,6 +30,16 @@ last_button:    ds 1;used inside checkbuttons
 current_button: ds 1
 button_store:   ds 1
 rand_int:       ds 1
+temp1:          ds 1
+
+p1_track_high:     ds 1
+p1_track_low:     ds 1
+p2_track_high:     ds 1
+p2_track_low:     ds 1
+p_total_track_high: ds 1  
+p_total_track_low: ds 1  
+led_track:    ds 1
+led_edge_track: ds 1
 
         
         CSEG
@@ -64,6 +76,25 @@ init:   mov A, P2 ;DIP switches
         anl A, player2
         mov P5, A
 
+
+        mov p1_track_high, #00h
+        mov p1_track_low, #00h
+        mov p2_track_high, #00h
+        mov p2_track_low, #00h
+        mov p_total_track_high, #00h  
+        mov p_total_track_low, #00h
+        mov led_track, #00h
+        mov led_edge_track, #00h
+        mov last_button, #00h
+        mov current_button, #00h
+        mov button_store, #00h
+        mov p1_track_high, #00h
+        mov p2_track_high, #01h
+        mov p2_track_low, #00h
+        mov p1_track_low, #80h
+  
+        call disp_update
+
         call delay_1000ms;1s delay for start of game
 
         mov A, player1;split the players apart
@@ -73,19 +104,30 @@ init:   mov A, P2 ;DIP switches
         rl A ;rotate left
         mov player2, A
 
-        mov A, player1
-        anl A, player2
-        mov P5, A
-
-;-------------- Main Game Code --------------------------   
-main:   call delay
-        jmp check_buttons
         
 
-disp_upt:;should we change this to update display?     
-        mov A, player1
-        anl A, player2
-        mov P5, A 
+
+        
+
+;-------------- Main Game Code --------------------------   
+main:   
+        
+        
+        call check_buttons
+        call delay
+        call split
+
+        mov A, p_total_track_low
+        cjne A, #18h, check_high
+        jmp init
+        
+check_high:
+        mov A, p_total_track_high
+        cjne A, #18h, proceed
+        jmp init
+
+proceed:
+        call disp_update 
        
 ; Check if game is in a winning state
         mov last_button, button_store
@@ -101,51 +143,151 @@ check_buttons:  MOV A, P1
 
                 
 cp0:            cjne A, #00h, cp1
-                jmp main
+                ret
 
 cp1:            cjne A, #01h, cp2
                 call roll_p1
-                jmp disp_upt
+                call disp_update
+                ret
 
 cp2:            cjne A, #02h, cp3
                 call roll_p2
-                jmp disp_upt
+                call disp_update
+                ret
 
 cp3:            mov  A, last_button  
                 cjne A, #03h, scp1
-                jmp main
+                call main
+                ret
                 
 scp1:           cjne A, #02h, scp2
                 call roll_p2
-                jmp disp_upt
+                call disp_update
+                ret
 
 scp2:           cjne A, #01h, scp3
                 call roll_p1
-                jmp disp_upt
+                call disp_update
+                ret
 
 scp3:           call roll_p1               
                 call roll_p2
-                jmp disp_upt
-
+                call disp_update
+                ret
 
                 RET
+
+
 ;------------- Roll P1 Sub ------------------------------
-roll_p1:cjne A, #01h, roll_p2 
-        mov A, player1
-        rl A
-        mov player1, A
+roll_p1:
+        mov A, p1_track_low
+        mov B, p1_track_high
+        rlc A
+        xch A, B
+        rlc A
+        xch A, B
+        mov p1_track_low, A
+        mov p1_track_high, B
         ret
 
 
 ;------------- Roll P2 Sub ------------------------------
-roll_p2:cjne A, #02h, main
-        mov A, player2
-        rr A
-        mov player2, A
+roll_p2:
+        mov A, p2_track_high
+        mov B, p2_track_low
+        rrc A
+        xch A, B
+        rrc A
+        xch A, B
+        mov p2_track_high, A
+        mov p2_track_low, B
+        ret
+
+
+;------------- Roll P1 Sub for Split ------------------------------
+roll_p1_split:
+        mov A, p1_track_high
+        mov B, p1_track_low
+        rrc A
+        xch A, B
+        rrc A
+        xch A, B
+        mov p1_track_high, A
+        mov p1_track_low, B
+        ret
+
+
+;------------- Roll P2 Sub for Split ------------------------------
+roll_p2_split:
+        mov A, p2_track_low
+        mov B, p2_track_high
+        rlc A
+        xch A, B
+        rlc A
+        xch A, B
+        mov p2_track_low, A
+        mov p2_track_high, B
+        ret
+
+;------------ 16 anding sub ------------------------------
+big_oring: 
+        mov A, p1_track_high
+        orl A, p2_track_high
+        mov p_total_track_high, A
+        mov A, p1_track_low
+        orl A, p2_track_low
+        mov p_total_track_low, A
         ret
 
 ;------------- Update LEDs Subroutine -------------------
-; Reference Main
+
+disp_update:
+        call big_oring
+; Middle 8-bits masking
+        mov A, p_total_track_high
+        anl A, #0Fh
+        rl A
+        rl A
+        rl A
+        rl A
+        mov led_track, A
+        mov A, p_total_track_low
+        anl A, #11110000b
+        rr A
+        rr A
+        rr A
+        rr A
+        orl A, led_track
+        mov led_track, A
+
+; Edge masking
+        mov A, p_total_track_high
+        anl A, #10h
+        rr A
+        rr A
+        rr A
+        mov led_edge_track, A
+        mov A, p_total_track_low
+        anl A, #08h
+        rr A
+        rr A
+        rr A
+        orl A, led_edge_track
+        mov led_edge_track, A
+
+        mov A, led_track
+        cpl A
+        mov P5, A
+        mov A, led_edge_track
+        cpl A
+        mov P3, A
+        
+        ret
+
+
+
+
+
 
 ;------------- Look Up Table ----------------------------
 ; The follow table consists of: 11111110, 11111101, 11111011, 11110111, 11101111, 11011111, 10111111, 01111111
@@ -177,6 +319,44 @@ here4:  djnz R3, here4
         djnz R4, here3
         jmp here5
 
+;--------------- Split Routine ------------------
+split:  mov A, led_track
+        cjne A, #18h, start_check
+        call split_actual
+        ret
+
+start_check:        
+        mov R6, #00h
+        mov A, p_total_track_high
+css1:   cjne A, #03h, cs1
+        jmp split_actual
+cs1:
+        cjne R6, #03h, cs11
+        jmp part2
+cs11:  inc R6     
+        rr A
+        jmp css1
+
+
+part2:  mov R6, #00h
+        mov A, p_total_track_low
+css2:   cjne A, #03h, cs2
+        jmp split_actual
+cs2:
+        cjne R6, #07h, cs22
+        jmp not_next_to_each_return
+cs22:  inc R6     
+        rr A
+        jmp css2
+
+
+split_actual:
+        call roll_p1_split
+        call roll_p2_split
+        ret
+
+
+not_next_to_each_return: ret
 
 loop1:  jmp loop1
         END
