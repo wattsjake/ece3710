f9c5e1d3d531769a3e8f7aab43f75c5dfceb2142
Jack <jackfernald@mail.weber.edu>
2023-02-22 22:08:53 -0700

    working on random delay

--- ece3710-project01-wattsjake/src/sumo.asm
+++ ece3710-project01-wattsjake/src/sumo.asm
@@ -76,6 +76,19 @@ init:   mov A, P2 ;DIP switches
         anl A, player2
         mov P5, A
 
+        mov	oscxcn,#67H	; turn on external crystal
+      	mov	tmod,#20H		; wait 1ms using T1 mode 2
+      	mov	th1,#256-167	; 2MHz clock, 167 counts = 1ms
+      	setb	tr1
+wait1:
+      	jnb	tf1,wait1
+      	clr	tr1			; 1ms has elapsed, stop timer
+      	clr	tf1
+wait2:
+      	mov	a,oscxcn		; now wait for crystal to stabilize
+      	jnb	acc.7,wait2
+      	mov	oscicn,#8		; engage! Now using 22.1184MHz
+
 
         mov p1_track_high, #00h
         mov p1_track_low, #00h
@@ -295,29 +308,13 @@ init_table: DB 0FEh, 0FDh, 0FBh, 0F7h, 0EFh, 0DFh, 0BFh, 07Fh
 
 ext_table: DB 0FFh
 
-;------------- Randoom Delay Subroutine ------------------
+;------------- Delay Subroutine ------------------
 delay:
         djnz R7,carry_on 
         mov R7,#50 
 carry_on:
-        MOV R4, #50 ;about 17.20 ms
-here1:	MOV R3, #250			
-here2:	DJNZ R3, here2
-        DJNZ R4, here1
-        RET
+        
 
-;-------------------- 1000ms Delay -----------------------
-delay_1000ms:
-        mov R5, #20 ; about 1000ms
-here5:  djnz R5, delay_50ms
-        RET
-
-delay_50ms:
-        mov R4, #150 ;about 51.60 ms
-here3:  mov R3, #250
-here4:  djnz R3, here4
-        djnz R4, here3
-        jmp here5
 
 ;--------------- Split Routine ------------------
 split:  mov A, led_track
