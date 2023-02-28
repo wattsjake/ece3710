accfa6b2390f5a68a46524bda3bd07676c53ce0d
wattsjake <watts.jacob.samuel@gmail.com>
2023-02-27 17:00:59 -0700

    finished magic

--- ece3710-lab04-wattsjake/src/ams/magic.asm
+++ ece3710-lab04-wattsjake/src/ams/magic.asm
@@ -26,6 +26,7 @@ $include (c8051f020.inc)
 current_button: ds 1
 last_button:    ds 1
 led_position:   ds 1
+rand_int:       ds 1
 
 ;------------------------ CSEG --------------------------       
         CSEG
@@ -68,34 +69,25 @@ init:
 ;--------------------- Main Code ------------------------
 main:
         call delay_10ms
-        mov A, R1
-        mov led_position, A
         call check_buttons
         mov A, current_button
         cjne A, last_button, cont
-            jmp main
+            jmp serial_check
             
 cont:   cjne A, #001h, serial_check ;if button pressed jmp
             jmp tx_sub
 
 serial_check:
-        jbc RI1, tx_sub
-        jmp main
+        jnb ri, main ;jump to main if ri not set
+        clr ri
+        jmp tx_sub
 
-;---------------------- LED Display ---------------------
-update_disp:
-        mov A, led_position
-        mov dptr, #led_table
-        movc A, @A+dptr
-        mov P5, A
-        clr A
-        clr C
-        ret
 ;------------------------- tx ---------------------------
 tx_sub:
         call update_disp
+        call get_address
+
         setb TR1
-        mov DPTR, #msg_test
 fn:     clr A
         movc A, @A+DPTR
         jz main
@@ -108,27 +100,73 @@ here1:  jnb TI, here1
         clr TI
         ret                
 
+;---------------------- LED Display ---------------------
+update_disp:
+                mov led_position, rand_int
+                mov A, led_position
+update_p3_0:    cjne A, #0Ah, update_p3_1
+                    mov A, #02h
+                    mov P3, A
+                    mov A, #0FFh
+                    mov P5, A
+                    ret
+
+update_p3_1:    cjne A, #01h, update_p5
+                    mov A, #01h
+                    mov P3, A
+                    mov A, #0FFh
+                    mov P5, A
+                    ret
+        
+update_p5:      mov dptr, #led_table
+                movc A, @A+dptr
+                mov P5, A
+                mov A, #0FFh
+                mov P3, A
+        
+                ret
+;-------------------- get string addr --------------------
+get_address:
+            mov A, rand_int
+            rl A
+            mov B, A
+            mov dptr, #msg_idx
+            movc A, @a+dptr ;msb of address
+            xch a,b
+            inc a
+            movc a,@a+dptr
+            mov dpl, a
+            mov dph, b
+            clr a
+            clr c
+
+            ret
+
 ;-------------------- message table ----------------------
-msg_test: DB "Hello World",0Dh,0Ah, 0 ;termination is zero (0)
-  
-;message_table1: DB "It is certain", "You may rely on it", "Without a doubt", "Yes", "Most likely", "Reply hazy, try again", "Concentrate and ask again"  
-;message_table2: DB "Don't count on it", "Very doubtful", "My reply is no" 
+msg_1: DB "It is certain", 0Dh, 0Ah, 0
+msg_2: DB "You may rely on it", 0Dh, 0Ah, 0
+msg_3: DB "Without a doubt", 0Dh, 0Ah, 0
+msg_4: DB "Yes", 0Dh, 0Ah, 0
+msg_5: DB "Most likely", 0Dh, 0Ah, 0
+msg_6: DB "Reply hazy, try again", 0Dh, 0Ah, 0
+msg_7: DB "Concentrate and ask again", 0Dh, 0Ah, 0
+msg_8: DB "Don't count on it", 0Dh, 0Ah, 0
+msg_9: DB "Very doubtful", 0Dh, 0Ah, 0
+msg_10: DB "My reply is no", 0Dh, 0Ah, 0
+
+;------------------- msg idx table ----------------------
+msg_idx: DW msg_1, msg_2, msg_3, msg_4, msg_5, msg_6, msg_7, msg_8, msg_9, msg_10
 
 ;--------------------- LED table -------------------------
-led_table: DB 0FEh, 0FDh, 0FBh, 0F7h, 0EFh, 0DFh, 0BFh, 07Fh
-
-;------------- Randoom Delay Subroutine ------------------
-;TODO
+led_table: DB 0FFh, 0FFh, 0FEh, 0FDh, 0FBh, 0F7h, 0EFh, 0DFh, 0BFh, 07Fh
 
 ;------------------- Check Buttons ----------------------
-check_buttons: 	MOV A, P1
-                MOV last_button, current_button
-                CPL A
-                XCH A, current_button
-                XRL A, current_button
-                ANL A, current_button
-                
-                MOV current_button, A
+check_buttons:  MOV A, P1
+                MOV last_button, current_button ; Memory for next loop
+                CPL A ; Complement A
+                ANL A, #001h ; Mask out other bits from P1, we only care about the single button bit. Anding it with 1 will keep the state of the button
+                ; e.g. button = 1 and with 1 = 1, button = 0 and with 1 = 0. T
+                MOV current_button, A ; Move this newly found button state into current_button
                 RET
 
 ;--------------------- 10ms Delay ------------------------
@@ -142,6 +180,11 @@ here:       mov TL0, #000h ;-9216 for 5ms
 again:      jnb TF0, again
             clr TR0
             clr TF0
+
+            mov A, R1
+            mov rand_int, A
+            clr A; for good measure
+            clr C; for good measure
             ret
 
-        END
+            END
